7200ab8559b199055b938302aea20a66ddcfc287
unknown <xiaoji.lin@crop.trjcn.com>
2018-06-26 11:41:01 +0800

    还原videoview，添加加载动画

--- app/src/main/java/com/encartoon/encartoon/PlayActivity.java
+++ app/src/main/java/com/encartoon/encartoon/PlayActivity.java
@@ -2,7 +2,14 @@ package com.encartoon.encartoon;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.ProgressBar;
+import android.widget.VideoView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -28,16 +35,23 @@ public class PlayActivity extends Activity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String s) {
-//                        Uri uri = Uri.parse(s);
-//                        VideoView videoView = findViewById(R.id.videoView);
-//                        videoView.setMediaController(new MediaController(PlayActivity.this));
-//                        videoView.setVideoURI(uri);
-//                        videoView.start();
-//                        videoView.requestFocus();
+                        Uri uri = Uri.parse(s);
+                        final VideoView videoView = findViewById(R.id.videoView);
+                        videoView.setMediaController(new MediaController(PlayActivity.this));
+                        videoView.setVideoURI(uri);
+                        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                            @Override
+                            public void onPrepared(MediaPlayer mediaPlayer) {
+                                ProgressBar progressBar = findViewById(R.id.progressBar);
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        });
+                        videoView.start();
+                        videoView.requestFocus();
 
-                        JZVideoPlayerStandard jzVideoPlayerStandard = (JZVideoPlayerStandard) findViewById(R.id.videoPlayer);
-                        jzVideoPlayerStandard.setUp(s, JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL, " ");
-                        jzVideoPlayerStandard.startButton.callOnClick();
+//                        JZVideoPlayerStandard jzVideoPlayerStandard = (JZVideoPlayerStandard) findViewById(R.id.videoPlayer);
+//                        jzVideoPlayerStandard.setUp(s, JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL, " ");
+//                        jzVideoPlayerStandard.startButton.callOnClick();
 
                         //jzVideoPlayerStandard.thumbImageView.setImage("http://p.qpic.cn/videoyun/0/2449_43b6f696980311e59ed467f22794e792_1/640");
                     }
